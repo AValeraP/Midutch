b5a3a1014dd559ab2f6168b36465acb9872ee61f
Alex <avaleraprieto@gmail.com>
2026-03-23 17:41:16 +0100

    feat: added streamers on json and recorred with a v-for

--- app/components/Home/HomeComp.vue
+++ app/components/Home/HomeComp.vue
@@ -1,6 +1,8 @@
 <script setup>
 import TheHeader from '../TheHeader.vue';
+import RecommendedChannels from '../RecommendedChannels.vue';
 </script>
 <template>
     <TheHeader></TheHeader>
+    
 </template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,12 @@
 <script setup>
 </script>
 <template>
-    <TheHeader></TheHeader>
+    <header>
+        <TheHeader></TheHeader>
+    </header>
+    <div class="bg-stone-950 min-h-screen flex">
+    <main>
+        <RecommendedChannels></RecommendedChannels>
+    </main>
+    </div>
 </template>
